f5b3be038c1d91aa88f2ca83d31792fa43a24da4
codeonoto <onoto0409@gmail.com>
2024-02-17 19:58:15 +0530

    first commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton 
+        afterSignOutUrl="/"
+      />
+      
+      <ModeToggle />
+    </div>
+  )
+}
